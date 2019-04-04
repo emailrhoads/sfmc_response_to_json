e5d96a79c941b1092bbe5d19478ee3f942509074
John Rhoads <john.rhoads@pnmac.com>
2019-04-04 14:55:30 -0700

    Minor cleanup of README

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+import pathlib
 from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
     name='sfmc-to-json',
-    version="0.2.4",
+    version="0.2.5",
     description="Used to format FuelSDK reponses into JSON",
-    url="https://github.com/realpython/reader",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/emailrhoads/sfmc_response_to_json",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
@@ -14,4 +23,5 @@ setup(
     ],
     packages=["salesforce_to_json"],
     include_package_data=True,
-    install_requires=["suds"
+    install_requires=["suds"]
+)
