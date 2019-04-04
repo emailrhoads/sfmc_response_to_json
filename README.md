96517c5b728bdcdb394ba35aa9e4fac974ffdff6
John Rhoads <john.rhoads@pnmac.com>
2019-04-04 15:02:57 -0700

    Fix to README.md

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Salesforce Marketing Cloud -- Response to JSON
 
-This is a small utility library that I built to process responses from the Salesforce Marketing Cloud SDK. It returns JSON payloads sometimes and other times it will return a list of SUDS objects.
+This is a small utility library that I built to process responses from the [Salesforce Marketing Cloud SDK](https://github.com/salesforce-marketingcloud/FuelSDK-Python). It returns JSON payloads sometimes and other times it will return a list of SUDS objects.
 
 ## Installation
 
--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
-import pathlib
+from os import path
 from setuptools import setup
-
-# The directory containing this file
-HERE = pathlib.Path(__file__).parent
-
-# The text of the README file
-README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
     name='sfmc-to-json',
     version="0.2.5",
     description="Used to format FuelSDK reponses into JSON",
-    long_description=README,
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/emailrhoads/sfmc_response_to_json",
     license="MIT",
